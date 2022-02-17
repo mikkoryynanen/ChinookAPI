28a190c7f4b631c0547ac315c8a05579c275e76e
Mikko <mikko.ryynanen2@gmail.com>
2022-02-17 20:31:52 +0200

    FEAT: Added Chinook repository and few base methods

---
+++ ChinookAPI/Repositories/ChinookRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ChinookAPI.Models;
+using ChinookAPI.Repositories;
+
+namespace ChinookAPI.Repositories
+{
+    public class ChinookRepository: IChinookRepository
+    {
+        public bool CreateCustomer(Customer newCustomer)
+        {
+            try
+            {
+                using (ChinookContext context = new ChinookContext())
+                {
+                    context.Customers.Add(newCustomer);
+                    return context.SaveChanges() > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
+        }
+
+        public List<Customer> FindMatchingCustomerWithName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetHighestSpendingCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Genre> GetMostPopularGenreForCustomer(int customerId)
+        {
+            return new List<Genre>();
+        }
+
+        public List<Customer> GetNumberOfCustomers(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, int> GetUserCountPerCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            using (ChinookContext context = new ChinookContext())
+            {
+                return context.Customers.ToList();
+            }
+        }
+
+        public bool UpdateCustomer(int customerId, Customer updatedCustomerData)
+        {
+            using (ChinookContext context = new ChinookContext())
+            {
+                Customer foundCustomer = context.Customers.First(customer => customer.CustomerId == customerId);
+                if (foundCustomer != null)
+                {
+                    foundCustomer.FirstName = updatedCustomerData.FirstName;
+                    foundCustomer.LastName = updatedCustomerData.LastName;
+                    foundCustomer.Company = updatedCustomerData.Company;
+                    foundCustomer.Address = updatedCustomerData.Address;
+                    foundCustomer.City = updatedCustomerData.City;
+                    foundCustomer.State = updatedCustomerData.State;
+                    foundCustomer.Country = updatedCustomerData.Country;
+                    foundCustomer.PostalCode = updatedCustomerData.PostalCode;
+                    foundCustomer.Phone = updatedCustomerData.Phone;
+                    foundCustomer.Fax = updatedCustomerData.Fax;
+                    foundCustomer.Email = updatedCustomerData.Email;
+                    foundCustomer.SupportRepId = updatedCustomerData.SupportRepId;
+                    foundCustomer.SupportRep = updatedCustomerData.SupportRep;
+                    foundCustomer.Invoices = foundCustomer.Invoices;
+                }
+
+                return context.SaveChanges() > 0;
+            }
+        }
+    }
+}
---
+++ ChinookAPI/Repositories/IChinookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ChinookAPI.Models;
+
+namespace ChinookAPI.Repositories
+{
+    public interface IChinookRepository
+    {
+        List<Customer> GetAllCustomers();
+        List<Customer> GetNumberOfCustomers(int limit, int offset);
+        Customer GetCustomer(int customerId);
+        List<Customer> FindMatchingCustomerWithName(string name);
+        bool CreateCustomer(Customer newCustomer);
+        bool UpdateCustomer(int customerId, Customer updatedCustomerData);
+        Dictionary<string, int> GetUserCountPerCountry();
+        List<Customer> GetHighestSpendingCustomers();
+        List<Genre> GetMostPopularGenreForCustomer(int customerId);
+    }
+}
