47851c199a5eab1f5e9edd7fcdc8bdd8bad4deec
Mikko <mikko.ryynanen2@gmail.com>
2022-02-17 20:25:46 +0200

    FEAT: Added generated Models from database

---
+++ ChinookAPI/Models/Playlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ChinookAPI.Models
+{
+    public partial class Playlist
+    {
+        public Playlist()
+        {
+            PlaylistTracks = new HashSet<PlaylistTrack>();
+        }
+
+        public int PlaylistId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<PlaylistTrack> PlaylistTracks { get; set; }
+    }
+}
