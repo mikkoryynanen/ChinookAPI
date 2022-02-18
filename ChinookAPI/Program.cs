fc5b4ab2f47bb77f3225db406f640f563a49291a
Mikko <mikko.ryynanen2@gmail.com>
2022-02-18 10:25:45 +0200

    CHORE: Added functions to test database integration

--- ChinookAPI/Program.cs
+++ ChinookAPI/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using ChinookAPI.Misc;
+using ChinookAPI.Models;
+using ChinookAPI.Repositories;
+using Microsoft.Data.SqlClient;
 
 namespace ChinookAPI
 {
@@ -6,7 +12,25 @@ namespace ChinookAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            IChinookRepository repository = new ChinookRepository();
+            //if(repository.CreateCustomer(new Customer { FirstName = "Hello", LastName = "World" , Email = "google@google.com"}))
+            //{
+            //    Console.WriteLine("customer generated");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("customer generation failed");
+            //}
+
+            var customers = repository.GetAllCustomers();
+            foreach (var c in customers)
+            {
+                Console.WriteLine(ConsumerInformationPrinter.BuildConsumerData(c));
+            }
+
+            //var c = repository.GetCustomer(1);
+            //Console.WriteLine(c);
         }
     }
 }
