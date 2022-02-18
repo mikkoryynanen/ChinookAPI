0a4a6833ceacf81b545c12f6a8303d2f50244981
Mikko <mikko.ryynanen2@gmail.com>
2022-02-18 10:26:37 +0200

    FEAT: Added ConsumerInformationPrinter

---
+++ ChinookAPI/Misc/ConsumerInformationPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using ChinookAPI.Models;
+
+namespace ChinookAPI.Misc
+{
+    public class ConsumerInformationPrinter
+    {
+        public static string BuildConsumerData(Customer customer)
+        {
+            StringBuilder costumerStr = new StringBuilder();
+            costumerStr.AppendLine($"ID: {customer.CustomerId}");
+            costumerStr.AppendLine($"Name: {customer.FirstName} {customer.LastName}");
+            costumerStr.AppendLine($"Country: {customer.Country}");
+            costumerStr.AppendLine($"Postal Code: {customer.PostalCode}");
+            costumerStr.AppendLine($"Phone: {customer.Phone}");
+            costumerStr.AppendLine($"Email: {customer.Email}");
+            costumerStr.AppendLine("");
+            return costumerStr.ToString();
+        }
+    }
+}
